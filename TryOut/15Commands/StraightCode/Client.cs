edb422e33b75a88d5ace4608431f408a5d11bc4d
Violet Tape <my@violet-tape.net>
2023-09-04 14:57:54 +0200

    update example  for commands

--- TryOut/15Commands/StraightCode/Client.cs
+++ TryOut/15Commands/StraightCode/Client.cs
@@ -1,20 +1,43 @@
 ﻿namespace TryOut.Commands.StraightCode {
-    public class Client {
-        public Client() {
-            var devastatorRemoteController = new DevastatorRemoteController();
-            var devastator1 = new Devastator(new Channel {Frequncy = "23S"});
-            devastatorRemoteController.Add(devastator1);
 
-            var devastator2 = new Devastator(new Channel {Frequncy = "88Q"});
-            devastatorRemoteController.Add(devastator2);
+        public class Client {
+            public Client() {
+                var notifications = new NotificationService();
+                var orderService = new OrderService();
+                var bomService = new BomService(); // BOM - Bill of materials
+                var processing = new ProcessingController(orderService, bomService, notifications);
 
-            devastatorRemoteController.SetChannel(devastator1.Channel);
-            devastatorRemoteController.MoveBackward();
-            devastatorRemoteController.Boost();
-
-            devastatorRemoteController.SetChannel(devastator2.Channel);
-            devastatorRemoteController.MoveLeft();
-            devastatorRemoteController.FlipUp();
+                var order = processing.PlaceOrder(new Product());
+            }
         }
-    }
+
+        public class NotificationService {
+            public void Send(Notification notification) {
+                Console.WriteLine("NotificationService - done");
+            }
+        }
+
+        public class OrderService {
+            public async Task<object> CreateOrder(Product product) {
+                Console.WriteLine("OrderService.CreateOrder");
+
+                return new object();
+            }
+
+            public async Task Evaluate(object materials) {
+                Console.WriteLine("OrderService.Evaluate");
+            }
+        }
+
+        public class BomService {
+            public async Task<object> GetMaterials(Product product) {
+                Console.WriteLine("BomService.GetMaterials");
+                return default;
+            }
+        }
+
+        public class Notification { }
+
+        public class Product { }
+    
 }
